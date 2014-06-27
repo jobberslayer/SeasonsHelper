bd055a3a3392ef2d6f4267aa14506b0d6207633b
Kevin Lester <kevin@mini.local>
2014-06-27 16:57:53 -0400

    first

--- SeasonsHelper/ViewController.h
+++ SeasonsHelper/ViewController.h
@@ -9,5 +9,28 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UISlider *incSlider;
+@property (weak, nonatomic) IBOutlet UILabel *incLabel;
+@property (weak, nonatomic) IBOutlet UIStepper *scoreStepper;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UISlider *summonSlider;
+@property (weak, nonatomic) IBOutlet UILabel *summonLabel;
+@property NSInteger currentPlayer;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *whichPlayer;
+@property (weak, nonatomic) IBOutlet UIImageView *Player1Logo;
+@property (weak, nonatomic) IBOutlet UILabel *player1Label;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *NumOfPlayers;
+
+- (void)adjustPlayerLogo;
+
+- (IBAction)incChanged:(UISlider *)sender;
+- (IBAction)scoreChanged:(UIStepper *)sender;
+- (IBAction)summonChanged:(UISlider *)sender;
+- (IBAction)playerChanged:(UISegmentedControl *)sender;
+
+- (IBAction)nextTurn:(id)sender;
+- (IBAction)prevTurn:(id)sender;
+
 
 @end
--- SeasonsHelper/ViewController.m
+++ SeasonsHelper/ViewController.m
@@ -18,6 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _currentPlayer = 1;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +27,62 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)adjustPlayerLogo {
+    NSString *v = [_whichPlayer titleForSegmentAtIndex:[_whichPlayer selectedSegmentIndex]];
+    NSString *cp = [NSString stringWithFormat:@"%d", _currentPlayer];
+    
+    if ([v isEqualToString:cp]) {
+        [_Player1Logo setHidden: NO];
+    } else {
+        [_Player1Logo setHidden:YES];
+    }
+}
+
+- (IBAction)incChanged:(UISlider *)sender {
+    int v = [_incSlider value];
+    [_incLabel setText:[NSString stringWithFormat:@"%d", v]];
+    _scoreStepper.stepValue = v;
+    
+}
+
+- (IBAction)scoreChanged:(UIStepper *)sender {
+    int v = [_scoreStepper value];
+    [_scoreLabel setText:[NSString stringWithFormat:@"%d", v]];
+}
+
+- (IBAction)summonChanged:(UISlider *)sender {
+    int v = [_summonSlider value];
+    
+    [_summonLabel setText:[NSString stringWithFormat:@"%d", v]];
+}
+
+- (IBAction)playerChanged:(UISegmentedControl *)sender {
+    [self adjustPlayerLogo];
+    
+}
+
+- (IBAction)nextTurn:(id)sender {
+    NSString *v = [_NumOfPlayers titleForSegmentAtIndex:[_NumOfPlayers selectedSegmentIndex]];
+    int numPlayers = [v integerValue];
+    _currentPlayer = _currentPlayer + 1;
+    if (_currentPlayer > numPlayers) {
+        _currentPlayer = 1;
+    }
+    
+    [_player1Label setText:[NSString stringWithFormat:@"%d", _currentPlayer]];
+    [self adjustPlayerLogo];
+    
+}
+
+- (IBAction)prevTurn:(id)sender {
+    NSString *v = [_NumOfPlayers titleForSegmentAtIndex:[_NumOfPlayers selectedSegmentIndex]];
+    int numPlayers = [v integerValue];
+    _currentPlayer = _currentPlayer - 1;
+    if (_currentPlayer < 1) {
+        _currentPlayer = numPlayers;
+    }
+    
+    [_player1Label setText:[NSString stringWithFormat:@"%d", _currentPlayer]];
+    [self adjustPlayerLogo];
+}
 @end
